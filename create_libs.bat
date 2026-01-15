fe11753eae8f6e9b2dfb5a3bf71449e4775de3f5
林奇 <tlqtangok@126.com>
2026-01-15 12:34:19 +0800

    smaller lib

--- create_libs.bat
+++ create_libs.bat
@@ -14,11 +14,7 @@ if /i "%1"=="rel" set BUILD_TYPE=release
 :set_config
 REM Define compiler options for both configurations
 REM Debug: Keep debug info but optimize for size
-::set VS_CL_OPT_DEBUG=/c /Z7 /nologo /W3 /O1 /Oy /Gy /Gw /D DEBUG /D _CONSOLE /EHsc /MDd /Zc:inline /permissive- /TP
-::set VS_LIB_OPT_DEBUG=/NOLOGO /MACHINE:X64
-::set VS_LINK_OPT_DEBUG=/MANIFEST /manifest:embed /DEBUG:FASTLINK /OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
-
-set VS_CL_OPT_DEBUG=/c /Zi /nologo /W3 /sdl /O1 /Gy /Gw /D DEBUG /D _CONSOLE /EHsc /MDd /Zc:inline /permissive- /TP
+set VS_CL_OPT_DEBUG=/c /Zi /nologo /W3 /O1 /Oy /Gy /Gw /D DEBUG /D _CONSOLE /EHsc /MDd /Zc:inline /permissive- /TP
 set VS_LIB_OPT_DEBUG=/NOLOGO /MACHINE:X64
 set VS_LINK_OPT_DEBUG=/MANIFEST /manifest:embed /DEBUG:FASTLINK /OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
 
@@ -26,13 +22,10 @@ REM Release: Maximum optimization for smallest size
 REM /O1 = Minimize size, /Oi = Intrinsic functions, /Oy = Omit frame pointers
 REM /GL = Whole program optimization, /GS- = Disable security checks, /Gy = Function-level linking
 REM /Gw = Optimize global data, /GF = String pooling
-::set VS_CL_OPT_RELEASE=/c /Zi /nologo /W3 /O1 /GL /GS- /Gy /Gw /GF /D NDEBUG /D _CONSOLE /EHsc /MD /Zc:inline /permissive- /TP
-::set VS_LIB_OPT_RELEASE=/NOLOGO /MACHINE:X64 /LTCG
-::set VS_LINK_OPT_RELEASE=/LTCG /OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
 
-set VS_CL_OPT_RELEASE=/c /Zi /nologo /W3 /sdl /O1 /Gy /Gw /D DEBUG /D _CONSOLE /EHsc /MD /Zc:inline /permissive- /TP
-set VS_LIB_OPT_RELEASE=/NOLOGO /MACHINE:X64 /LTCG
-set VS_LINK_OPT_RELEASE=/MANIFEST /manifest:embed  /OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
+set VS_CL_OPT_RELEASE=/c /nologo /W3 /O1 /Oi /Oy /GS- /Gy /Gw /GF /D NDEBUG /D _CONSOLE /EHsc /MD /Zc:inline /permissive- /TP
+set VS_LIB_OPT_RELEASE=/NOLOGO /MACHINE:X64
+set VS_LINK_OPT_RELEASE=/OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
 
 REM Set active configuration
 if "%BUILD_TYPE%"=="debug" (
