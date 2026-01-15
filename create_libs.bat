4e4b4e71eed52e0118c79900bfe2e76e9417e281
林奇 <tlqtangok@126.com>
2026-01-15 11:14:24 +0800

    create smaller lib

--- create_libs.bat
+++ create_libs.bat
@@ -13,16 +13,26 @@ if /i "%1"=="rel" set BUILD_TYPE=release
 
 :set_config
 REM Define compiler options for both configurations
-REM Debug: Minimal size while keeping debug symbols
-set VS_CL_OPT_DEBUG=/c /Zi /nologo /W3 /sdl /Od /Gy /Gw /D DEBUG /D _CONSOLE /EHsc /MDd /Zc:inline /permissive- /TP
+REM Debug: Keep debug info but optimize for size
+::set VS_CL_OPT_DEBUG=/c /Z7 /nologo /W3 /O1 /Oy /Gy /Gw /D DEBUG /D _CONSOLE /EHsc /MDd /Zc:inline /permissive- /TP
+::set VS_LIB_OPT_DEBUG=/NOLOGO /MACHINE:X64
+::set VS_LINK_OPT_DEBUG=/MANIFEST /manifest:embed /DEBUG:FASTLINK /OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
+
+set VS_CL_OPT_DEBUG=/c /Zi /nologo /W3 /sdl /O1 /Gy /Gw /D DEBUG /D _CONSOLE /EHsc /MDd /Zc:inline /permissive- /TP
 set VS_LIB_OPT_DEBUG=/NOLOGO /MACHINE:X64
 set VS_LINK_OPT_DEBUG=/MANIFEST /manifest:embed /DEBUG:FASTLINK /OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
 
 REM Release: Maximum optimization for smallest size
-::set VS_CL_OPT_RELEASE=/c /nologo /W3 /O1 /Oi /Gy /Gw /GL /GS- /D NDEBUG /D _CONSOLE /EHsc /MD /Zc:inline /permissive- /TP
-set VS_CL_OPT_RELEASE=/c /Zi /nologo /W3 /sdl /Od /Gy /Gw /D NDEBUG /D _CONSOLE /EHsc /MDd /Zc:inline /permissive- /TP
-set VS_LIB_OPT_RELEASE=/NOLOGO /MACHINE:X64 /LTCG
-set VS_LINK_OPT_RELEASE=/LTCG /OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
+REM /O1 = Minimize size, /Oi = Intrinsic functions, /Oy = Omit frame pointers
+REM /GL = Whole program optimization, /GS- = Disable security checks, /Gy = Function-level linking
+REM /Gw = Optimize global data, /GF = String pooling
+::set VS_CL_OPT_RELEASE=/c /Zi /nologo /W3 /O1 /GL /GS- /Gy /Gw /GF /D NDEBUG /D _CONSOLE /EHsc /MD /Zc:inline /permissive- /TP
+::set VS_LIB_OPT_RELEASE=/NOLOGO /MACHINE:X64 /LTCG
+::set VS_LINK_OPT_RELEASE=/LTCG /OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
+
+set VS_CL_OPT_RELEASE=/c /Zi /nologo /W3 /sdl /O1 /Gy /Gw /D DEBUG /D _CONSOLE /EHsc /MD /Zc:inline /permissive- /TP
+set VS_LIB_OPT_RELEASE=/NOLOGO /MACHINE:X64
+set VS_LINK_OPT_RELEASE=/MANIFEST /manifest:embed  /OPT:REF /OPT:ICF /SUBSYSTEM:CONSOLE /MACHINE:X64
 
 REM Set active configuration
 if "%BUILD_TYPE%"=="debug" (
@@ -65,8 +75,8 @@ if not %errorlevel% equ 0 (
 )
 
 echo.
-echo Step 2: Creating static library slib.lib...
-lib.exe %VS_LIB_OPT% /OUT:"slib.lib" format.obj os.obj
+echo Step 2: Creating static library fmt.lib...
+lib.exe %VS_LIB_OPT% /OUT:"fmt.lib" format.obj os.obj
 
 if not %errorlevel% equ 0 (
     echo ERROR: Failed to create library
@@ -84,7 +94,7 @@ if not %errorlevel% equ 0 (
 
 echo.
 echo Step 4: Linking mainapp.exe...
-link.exe %VS_LINK_OPT% /out:"mainapp.exe" main.obj slib.lib
+link.exe %VS_LINK_OPT% /out:"mainapp.exe" main.obj fmt.lib
 
 if not %errorlevel% equ 0 (
     echo ERROR: Failed to link mainapp.exe
@@ -97,7 +107,7 @@ echo SUCCESS! %CONFIG_NAME% Library and demo created
 echo ============================================
 echo.
 echo Files created:
-echo   - slib.lib (static library - %CONFIG_NAME%)
+echo   - fmt.lib (static library - %CONFIG_NAME%)
 echo   - mainapp.exe (test program)
 echo.
 echo Running mainapp.exe...
@@ -107,7 +117,7 @@ echo.
 echo.
 echo.
 echo To use in your projects:
-echo   1. Copy slib.lib to your project
+echo   1. Copy fmt.lib to your project
 echo   2. Add include path: /I"%INC%"
-echo   3. Link with: slib.lib
+echo   3. Link with: fmt.lib
 echo.
